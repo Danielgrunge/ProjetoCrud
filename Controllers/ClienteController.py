66076f459772f12bd4ed231b7016e03b055a155f
Danielgrunge <45153176+Danielgrunge@users.noreply.github.com>
2025-02-03 17:00:04 -0300

    teste

--- Controllers/ClienteController.py
+++ Controllers/ClienteController.py
@@ -5,7 +5,7 @@ import services.database as db;
 def incluir(cliente):
 
     db.curs.execute("""INSERT INTO cliente(clinome, cliidade, cliprofissao) VALUES (%s,%s,%s)""",
-    cliente.nome, cliente.idade, cliente.profissao)
+    (cliente.nome, cliente.idade, cliente.profissao))
 
     db.connection.commit()
     count = db.curs.rowcount
